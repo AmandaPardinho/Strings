57e666a551cfbdf3f8697e10462867e04dd8cea8
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-08 19:33:15 -0300

    Refactored

--- anagram.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-
-def is_anagram(str1, str2):
-    return Counter(str1) == Counter(str2)
-
-
-print(is_anagram('taste', 'state'))
-print(is_anagram("beach", "peach"))
