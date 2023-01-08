ba056d56e4d35001fc99e4d91f76b30a9d18304f
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-07 22:30:28 -0300

    Code to print a sentence multiple times

---
+++ print_multiple.py
@@ -0,0 +1,6 @@
+sentA = "You don't need to use a repeating structure to print multiple times\n"
+for i in range(10):
+    print(sentA)
+
+sentB = "It is simple to print the same sentence without having to type \"print(\"sentence\")\" every time either\n"
+print(sentB*4)
