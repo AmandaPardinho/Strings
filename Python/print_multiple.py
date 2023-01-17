c982b8ae2aff1d70c09177fded47ea25abf1245b
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-17 15:34:34 -0300

    Changes in range

--- Python/print_multiple.py
+++ Python/print_multiple.py
@@ -1,5 +1,5 @@
 sentA = "You don't need to use a repeating structure to print a string multiple times\n"
-for i in range(10):
+for i in range(4):
     print(sentA)
 
 sentB = "It is simple to print the same sentence without having to type \"print(\"sentence\")\" every time either\n"
