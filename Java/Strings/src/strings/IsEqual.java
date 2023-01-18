df45b07d45de6605e94f9887fe4c12a4393bec82
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-18 09:36:55 -0300

    Dividing the exercise into cases and starting to set up the explanation

--- Java/Strings/src/strings/IsEqual.java
+++ Java/Strings/src/strings/IsEqual.java
@@ -6,6 +6,8 @@ public class IsEqual {
                 \nIf we have two strings and we want to compare their contents, determining if they are equal, what 
                 would be the answer obtained in each of these cases?
                 """);
+
+        //Case 1
         String name1 = "Amanda";
         String nickname1 = "Amanda";
 
@@ -15,6 +17,7 @@ public class IsEqual {
             System.out.println("False: name1 and nickname1 are different");
         }
 
+        //Case 2
         String name2 = new String("Amanda");
         String nickname2 = "Amanda";
 
@@ -23,5 +26,9 @@ public class IsEqual {
         }else{
             System.out.println("\nFalse: name2 and nickname2 are different");
         }
+
+        System.out.println("""
+                Why does Case 1 have a different answer than Case 2?
+                """);
     }
 }
