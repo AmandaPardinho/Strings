16fec41176f2de54ed584db92edbb0c21abf5c2e
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-18 09:28:05 -0300

    Creating the IsEqual class and its code comparing strings

--- Java/Strings/src/strings/IsEqual.java
+++ Java/Strings/src/strings/IsEqual.java
@@ -1,2 +1,27 @@
-package strings;public class IsEqual {
+package strings;
+
+public class IsEqual {
+    public static void main(String[] args) {
+        System.out.println("""
+                \nIf we have two strings and we want to compare their contents, determining if they are equal, what 
+                would be the answer obtained in each of these cases?
+                """);
+        String name1 = "Amanda";
+        String nickname1 = "Amanda";
+
+        if(name1 == nickname1){
+            System.out.println("True: name1 and nickname1 are equal");
+        }else{
+            System.out.println("False: name1 and nickname1 are different");
+        }
+
+        String name2 = new String("Amanda");
+        String nickname2 = "Amanda";
+
+        if(name2 == nickname2){
+            System.out.println("\nTrue: name2 and nickname2 are equal");
+        }else{
+            System.out.println("\nFalse: name2 and nickname2 are different");
+        }
+    }
 }
