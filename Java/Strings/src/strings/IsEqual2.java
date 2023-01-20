7efa6509e452cf290c5b9b8a7eddb0154958b526
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-20 19:38:12 -0300

    Changes in explanation and corrections in the text

--- Java/Strings/src/strings/IsEqual2.java
+++ Java/Strings/src/strings/IsEqual2.java
@@ -4,18 +4,23 @@ public class IsEqual2 {
     public static void main(String[] args) {
         System.out.println("""
                 \nWhat is the difference from what was done before with respect to using the equals method? 
-                What is the difference from what was done before with respect to using the equals method? The equals 
-                method compares each of the given objects character by character. If both objects are exactly the same, 
-                i.e. have exactly the same characters in the same order, the method will show that both are equal.
                 """);
 
-        String word1 = "Raul";
-        String word2 = "Luar";
+        //Case 1
+
+        String word1 = "Amanda";
+        String word2 = "Amanda";
 
         if(word1.equals(word2)){
-            System.out.println("They are equals");
+            System.out.println("They are equal");
         }else{
-            System.out.println("They are differents");
+            System.out.println("They are different");
         }
+
+        System.out.println("""
+                \nThe equals method compares each of the given objects character by character. If both objects are exactly
+                the same i.e. have exactly the same characters in the same order, the method will show that both are 
+                equal.
+                """);
     }
 }
