a523f6149969dd71d6e0e62a47334dd7f2571017
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-21 18:01:43 -0300

    Finish case 2

--- Java/Strings/src/strings/IsEqual2.java
+++ Java/Strings/src/strings/IsEqual2.java
@@ -18,9 +18,31 @@ public class IsEqual2 {
         }
 
         System.out.println("""
-                \nThe equals method compares each of the given objects character by character. If both objects are exactly
-                the same i.e. have exactly the same characters in the same order, the method will show that both are 
-                equal.
+                \nThe equals method compares each of the given objects character by character. If both objects are 
+                exactly the same i.e. have exactly the same characters in the same order, the method will show that 
+                both are equal.
                 """);
+
+        //Case 2
+
+        String word3 = "Amanda";
+        String word4 = "amanda";
+
+        if(word3.equals(word4)){
+            System.out.println("They are equal");
+        }else{
+            System.out.println("They are different");
+        }
+
+        System.out.println("""
+                \nIn this second case, the words are still the same. However, the characters used to write them are 
+                different. Java distinguishes between upper case and lower case letters. Since in this case we have the 
+                first letter of the two strings different (upper case and lower case, respectively), the conditional 
+                structure will return that they are different. If you want to ignore this case distinction, you can do 
+                so using the equalsIgnoreCase method.
+                """);
+
+        
+
     }
 }
