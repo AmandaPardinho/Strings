6c5e1c7a4c0e512937653039c74032375191f892
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-22 17:07:06 -0300

    Continuing the code to check if it is an anagram

--- Java/Strings/src/strings/anagram.java
+++ Java/Strings/src/strings/anagram.java
@@ -1,7 +1,18 @@
 package strings;
 
+import java.util.Arrays;
+
 public class anagram {
     public static void main(String[] args) {
+        String word1 = "Celia";
+        String word2 = "Alice";
+
+        char[] anagram1 = word1.toCharArray();
+        char[] anagram2 = word2.toCharArray();
+
+        Arrays.sort(word1);
+        Arrays.sort(word2);
+
         
     }
 }
